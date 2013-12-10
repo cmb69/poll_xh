ca145ad9316138c68d7ab8d30c5882d1e70504ed
cmb69 <cmbecker69@gmx.de>
2013-12-10 13:27:55 +0000

    reformatted according to CS

--- admin.php
+++ admin.php
@@ -3,127 +3,160 @@
 /**
  * Back-End of Poll_XH
  *
- * Copyright (c) 2012, Christoph M. Becker (see license.txt)
+ * PHP versions 4 and 5
+ *
+ * @category  CMSimple_XH
+ * @package   Poll
+ * @author    Christoph M. Becker <cmbecker69@gmx.de>
+ * @copyright 2012-2013 Christoph M. Becker <http://3-magi.net>
+ * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @version   SVN: $Id$
+ * @link      http://3-magi.net/?CMSimple_XH/Poll_XH
  */
 
-
+/*
+ * Prevent direct access.
+ */
 if (!defined('CMSIMPLE_XH_VERSION')) {
     header('HTTP/1.0 403 Forbidden');
     exit;
 }
 
-
 /**
- * Returns (x)html plugin version information.
+ * Returns the plugin's about view.
  *
- * @return string
+ * @return string (X)HTML.
+ *
+ * @global array The paths of system files and folders.
+ *
+ * @todo Fix empty elements.
  */
-function poll_version() {
+function Poll_aboutView()
+{
     global $pth;
 
-    return '<h1><a href="http://3-magi.net/?CMSimple_XH/Poll_XH">Poll_XH</a></h1>'."\n"
-	    .tag('img src="'.$pth['folder']['plugins'].'poll/poll.png" width="128"'
-	    .' height="128" alt="Plugin icon" class="poll_plugin_icon"')
-	    .'<p style="margin-top: 1em">Version: '.POLL_VERSION.'</p>'."\n"
-	    .'<p>Copyright &copy; 2012 <a href="http://3-magi.net/">Christoph M. Becker</a></p>'."\n"
-	    .'<p class="poll_license">This program is free software: you can redistribute it and/or modify'
-	    .' it under the terms of the GNU General Public License as published by'
-	    .' the Free Software Foundation, either version 3 of the License, or'
-	    .' (at your option) any later version.</p>'."\n"
-	    .'<p class="poll_license">This program is distributed in the hope that it will be useful,'
-	    .' but WITHOUT ANY WARRANTY; without even the implied warranty of'
-	    .' MERCHAN&shy;TABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the'
-	    .' GNU General Public License for more details.</p>'."\n"
-	    .'<p class="poll_license">You should have received a copy of the GNU General Public License'
-	    .' along with this program.  If not, see'
-	    .' <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</p>'."\n";
-}
+    $version = POLL_VERSION;
+    $o = <<<EOT
+<h1><a href="http://3-magi.net/?CMSimple_XH/Poll_XH">Poll_XH</a></h1>
+<img src="{$pth['folder']['plugins']}poll/poll.png" width="128" height="128"
+     alt="Plugin icon" class="poll_plugin_icon" />
+<p style="margin-top: 1em">Version: $version</p>
+<p>Copyright &copy; 2012-2013
+    <a href="http://3-magi.net/">Christoph M. Becker</a></p>
+<p class="poll_license">
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+</p>
+<p class="poll_license">
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHAN&shy;TABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+</p>
+<p class="poll_license">
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see
+    <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.
+</p>
 
-
-/**
- * Returns requirements information.
- *
- * @return string
- */
-function poll_system_check() { // RELEASE-TODO
-    global $pth, $tx, $plugin_tx;
-
-    define('POLL_PHP_VERSION', '4.3.0');
-    $ptx = $plugin_tx['poll'];
-    $imgdir = $pth['folder']['plugins'].'poll/images/';
-    $ok = tag('img src="'.$imgdir.'ok.png" alt="ok"');
-    $warn = tag('img src="'.$imgdir.'warn.png" alt="warning"');
-    $fail = tag('img src="'.$imgdir.'fail.png" alt="failure"');
-    $o = '<h4>'.$ptx['syscheck_title'].'</h4>'
-	    .(version_compare(PHP_VERSION, POLL_PHP_VERSION) >= 0 ? $ok : $fail)
-	    .'&nbsp;&nbsp;'.sprintf($ptx['syscheck_phpversion'], POLL_PHP_VERSION)
-	    .tag('br')."\n";
-    foreach (array('date', 'pcre') as $ext) {
-	$o .= (extension_loaded($ext) ? $ok : $fail)
-		.'&nbsp;&nbsp;'.sprintf($ptx['syscheck_extension'], $ext).tag('br')."\n";
-    }
-    $o .= (!get_magic_quotes_runtime() ? $ok : $fail)
-	    .'&nbsp;&nbsp;'.$ptx['syscheck_magic_quotes'].tag('br').tag('br')."\n";
-    $o .= (strtoupper($tx['meta']['codepage']) == 'UTF-8' ? $ok : $warn)
-	    .'&nbsp;&nbsp;'.$ptx['syscheck_encoding'].tag('br').tag('br')."\n";
-    foreach (array('config/', 'css/', 'languages/') as $folder) {
-	$folders[] = $pth['folder']['plugins'].'poll/'.$folder;
-    }
-    $folders[] = poll_data_folder();
-    foreach ($folders as $folder) {
-	$o .= (is_writable($folder) ? $ok : $warn)
-		.'&nbsp;&nbsp;'.sprintf($ptx['syscheck_writable'], $folder).tag('br')."\n";
-    }
+EOT;
     return $o;
 }
 
+/**
+ * Returns the system check view.
+ *
+ * @return string (X)HTML.
+ *
+ * @global array The paths of system files and folders.
+ * @global array The localization of the core.
+ * @global array The localization of the plugins.
+ */
+function Poll_systemCheckView()
+{
+    global $pth, $tx, $plugin_tx;
+
+    $phpVersion = '4.3.0';
+    $ptx = $plugin_tx['poll'];
+    $imgdir = $pth['folder']['plugins'] . 'poll/images/';
+    $ok = tag('img src="' . $imgdir . 'ok.png" alt="ok"');
+    $warn = tag('img src="' . $imgdir . 'warn.png" alt="warning"');
+    $fail = tag('img src="' . $imgdir . 'fail.png" alt="failure"');
+    $o = '<h4>' . $ptx['syscheck_title'] . '</h4>' . PHP_EOL
+        . (version_compare(PHP_VERSION, $phpVersion) >= 0 ? $ok : $fail)
+        . '&nbsp;&nbsp;' . sprintf($ptx['syscheck_phpversion'], $phpVersion)
+        . tag('br') . PHP_EOL;
+    foreach (array('pcre') as $ext) {
+        $o .= (extension_loaded($ext) ? $ok : $fail)
+            . '&nbsp;&nbsp;' . sprintf($ptx['syscheck_extension'], $ext)
+            . tag('br') . PHP_EOL;
+    }
+    $o .= (!get_magic_quotes_runtime() ? $ok : $fail)
+        . '&nbsp;&nbsp;' . $ptx['syscheck_magic_quotes']
+        . tag('br') . tag('br') . PHP_EOL;
+    $o .= (strtoupper($tx['meta']['codepage']) == 'UTF-8' ? $ok : $warn)
+        . '&nbsp;&nbsp;' . $ptx['syscheck_encoding']
+        . tag('br') . tag('br') . PHP_EOL;
+    foreach (array('config/', 'css/', 'languages/') as $folder) {
+        $folders[] = $pth['folder']['plugins'] . 'poll/' . $folder;
+    }
+    $folders[] = Poll_dataFolder();
+    foreach ($folders as $folder) {
+        $o .= (is_writable($folder) ? $ok : $warn)
+            . '&nbsp;&nbsp;' . sprintf($ptx['syscheck_writable'], $folder)
+            . tag('br') . PHP_EOL;
+    }
+    return $o;
+}
 
 /**
  * Returns the available polls.
  *
  * @return array
  */
-function poll_polls() {
-    $dn = poll_data_folder(); // TODO: .htaccess for data folder
-    $files = glob($dn.'*.csv');
+function Poll_polls()
+{
+    $folder = Poll_dataFolder();
+    $files = glob($folder . '*.csv');
     $polls = array();
     foreach ($files as $file) {
-	$polls[] = basename($file, '.csv');
+        $polls[] = basename($file, '.csv');
     }
     return $polls;
 }
 
-
 /**
  * Returns the plugin's main administration view.
  *
- * @return string  The (X)HTML.
+ * @return string The (X)HTML.
  */
-function poll_plugin_admin() {
-    $o = '<div id="poll_admin">'."\n";
-    foreach (poll_polls() as $poll) {
-	$o .= '<h1>'.$poll.'</h1>'.poll_results_view($poll)."\n";
+function Poll_pluginAdminView()
+{
+    $o = '<div id="poll_admin">' . PHP_EOL;
+    foreach (Poll_polls() as $poll) {
+        $o .= '<h1>' . $poll . '</h1>' . PHP_EOL
+            . Poll_resultsView($poll) . PHP_EOL;
     }
-
-    $o .= '</div>'."\n";
+    $o .= '</div>' . PHP_EOL;
     return $o;
 }
-
 
 /**
  * Handle the plugin administration.
  */
-if (!empty($poll)) {
+if (isset($poll) && $poll == 'true') {
     $o .= print_plugin_admin('on');
     switch ($admin) {
-	case '':
-	    $o .= poll_version().tag('hr').poll_system_check();
-	    break;
-	case 'plugin_main':
-	    $o .= poll_plugin_admin();
-	    break;
-	default:
-	    $o .= plugin_admin_common($action, $admin, $plugin);
+    case '':
+        $o .= Poll_aboutView() . tag('hr') . Poll_systemCheckView();
+        break;
+    case 'plugin_main':
+        $o .= Poll_pluginAdminView();
+        break;
+    default:
+        $o .= plugin_admin_common($action, $admin, $plugin);
     }
 }
 
--- index.php
+++ index.php
@@ -3,280 +3,377 @@
 /**
  * Front-End of Poll_XH
  *
- * Copyright (c) 2012 Christoph M. Becker (see license.txt)
+ * PHP versions 4 and 5
+ *
+ * @category  CMSimple_XH
+ * @package   Poll
+ * @author    Christoph M. Becker <cmbecker69@gmx.de>
+ * @copyright 2012-2013 Christoph M. Becker <http://3-magi.net>
+ * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @version   SVN: $Id$
+ * @link      http://3-magi.net/?CMSimple_XH/Poll_XH
  */
 
-
+/*
+ * Prevent direct access.
+ */
 if (!defined('CMSIMPLE_XH_VERSION')) {
     header('HTTP/1.0 403 Forbidden');
     exit;
 }
 
-
+/**
+ * The version number.
+ */
 define('POLL_VERSION', '1beta3');
-
 
 define('POLL_TOTAL', '%%%TOTAL%%%');
 define('POLL_MAX', '%%%MAX%%%');
 define('POLL_END', '%%%END%%%');
 
-
 /**
- * Returns the key for accessing the correct numerus.
+ * Returns a language string depending on the number.
  *
- * @param string $key  The base key.
- * @param int $count
+ * @param string $key   A base key.
+ * @param int    $count A number.
+ *
  * @return string
+ *
+ * @global array The localization of the plugins.
  */
-function poll_numerus($key, $count) {
+function Poll_number($key, $count)
+{
     global $plugin_tx;
 
-    $key .= $count == 1 ? '_singular' :
-	    ($count >= 2 && $count <= 4 ? '_plural_2-4' : '_plural_5');
-    return $plugin_tx['poll'][$key];
+    if ($count == 1) {
+        $suffix = '_singular';
+    } elseif ($count >= 2 && $count <= 4) {
+        $suffix = '_plural_2-4';
+    } else {
+        $suffix = '_plural_5';
+    }
+    return $plugin_tx['poll'][$key . $suffix];
 }
-
 
 /**
  * Returns the path to the data folder.
  *
  * @return string
+ *
+ * @global array The paths of system files and folders.
+ * @global array The configuration of the plugins.
  */
-function poll_data_folder() {
+function Poll_dataFolder()
+{
     global $pth, $plugin_cf;
 
     $pcf = $plugin_cf['poll'];
-
     if ($pcf['folder_data'] == '') {
-	$fn = $pth['folder']['plugins'].'poll/data/';
+        $folder = $pth['folder']['plugins'] . 'poll/data/';
     } else {
-	$fn = $pth['folder']['base'].$pcf['folder_data'];
+        $folder = $pth['folder']['base'] . $pcf['folder_data'];
     }
-    if (substr($fn, -1) != '/') {$fn .= '/';}
-    if (file_exists($fn)) {
-	if (!is_dir($fn)) {
-	    e('cntopen', 'folder', $fn);
-	}
+    if (substr($folder, -1) != '/') {
+        $folder .= '/';
+    }
+    if (file_exists($folder)) {
+        if (!is_dir($folder)) {
+            e('cntopen', 'folder', $folder);
+        }
     } else {
-	if (!mkdir($fn, 0777, TRUE)) {
-	    e('cntwriteto', 'folder', $fn);
-	}
+        if (!mkdir($folder, 0777, true)) {
+            e('cntwriteto', 'folder', $folder);
+        }
     }
-    return $fn;
+    return $folder;
 }
 
-
 /**
- * Returns the poll data.
+ * Reads or writes a poll, and returns its data.
  *
- * @param string $name  The name of the poll.
- * @param array $recs  The new records.
+ * The function caches the data of a single poll internally.
+ *
+ * @param string $name    A poll name.
+ * @param array  $newData New poll data.
+ *
  * @return array
  */
-function poll_data($name, $ndata = NULL) {
-    static $cname = NULL, $data = NULL;
+function Poll_data($name, $newData = null)
+{
+    static $cname = null, $data = null;
 
-    $fn = poll_data_folder().$name.'.csv';
-    if (is_null($ndata)) {
-	if (is_null($data) || $name != $cname) {
-	    $cname = $name;
-	    $data = array('max' => 1, 'end' => 2147483647, 'total' => 0);
-	    if (($lines = file($fn)) !== FALSE) {
-		foreach ($lines as $line) {
-		    $rec = explode("\t", rtrim($line));
-		    switch ($rec[0]) {
-			case POLL_MAX:
-			    $data['max'] = $rec[1];
-			    break;
-			case POLL_END:
-			    $data['end'] = $rec[1];
-			    break;
-			case POLL_TOTAL:
-			    $data['total'] = $rec[1];
-			    break;
-			default:
-			    $data['votes'][$rec[0]] = isset($rec[1]) ? $rec[1] : 0;
-		    }
-		}
-	    }
-	}
+    $filename = Poll_dataFolder() . $name . '.csv';
+    if (!isset($newData)) {
+        if (!isset($data) || $name != $cname) {
+            $cname = $name;
+            $data = array('max' => 1, 'end' => 2147483647, 'total' => 0);
+            $lines = file($filename);
+            if ($lines !== false) {
+                foreach ($lines as $line) {
+                    $record = explode("\t", rtrim($line));
+                    switch ($record[0]) {
+                    case POLL_MAX:
+                        $data['max'] = $record[1];
+                        break;
+                    case POLL_END:
+                        $data['end'] = $record[1];
+                        break;
+                    case POLL_TOTAL:
+                        $data['total'] = $record[1];
+                        break;
+                    default:
+                        $data['votes'][$record[0]]
+                            = isset($record[1]) ? $record[1] : 0;
+                    }
+                }
+            }
+        }
     } else {
-	$cname = $name;
-	$data = $ndata;
-	$lines = array();
-	foreach ($data['votes'] as $key => $count) {
-	    $lines[] = $key."\t".$count;
-	}
-	$lines[] = POLL_MAX."\t".($data['max']);
-	$lines[] = POLL_END."\t".$data['end'];
-	$lines[] = POLL_TOTAL."\t".$data['total'];
-	if (($fh = fopen($fn, 'w')) === FALSE
-		|| fwrite($fh, implode("\n", $lines)."\n") === FALSE) {
-	    e('cntsave', 'file', $fn);
-	}
-	if ($fh !== FALSE) {fclose($fh);}
+        $cname = $name;
+        $data = $newData;
+        $lines = array();
+        foreach ($data['votes'] as $key => $count) {
+            $lines[] = $key . "\t" . $count;
+        }
+        $lines[] = POLL_MAX . "\t" . $data['max'];
+        $lines[] = POLL_END . "\t" . $data['end'];
+        $lines[] = POLL_TOTAL . "\t" . $data['total'];
+        if (($stream = fopen($filename, 'w')) === false
+            || fwrite($stream, implode(PHP_EOL, $lines) . PHP_EOL) === false
+        ) {
+            e('cntsave', 'file', $filename);
+        }
+        if ($stream !== false) {
+            fclose($stream);
+        }
     }
     return $data;
 }
 
-
 /**
  * Returns whether the poll has ended.
  *
- * @param string $name  The name of the poll.
+ * @param string $name A poll name.
+ *
  * @return bool
  */
-function poll_has_ended($name) {
-    $data = poll_data($name);
+function Poll_hasEnded($name)
+{
+    $data = Poll_data($name);
     return $data['end'] <= time();
 }
-
 
 /**
  * Returns whether the current user has already voted.
  *
+ * The current user is identified by a cookie and by his IP address.
+ *
+ * @param string $name A poll name.
+ *
  * @return bool
  */
-function poll_has_voted($name) {
-    if (isset($_COOKIE['poll_'.$name]) && $_COOKIE['poll_'.$name] == CMSIMPLE_ROOT) {
-	return TRUE;
+function Poll_hasVoted($name)
+{
+    if (isset($_COOKIE['poll_' . $name])
+        && $_COOKIE['poll_' . $name] == CMSIMPLE_ROOT
+    ) {
+        return true;
     }
-    $fn = poll_data_folder().$name.'.ips';
-    if (!file_exists($fn)) {touch($fn);}
-    if (($lines = file($fn)) === FALSE) {
-	e('cntopen', 'file', $fn);
-	return FALSE;
+    $filename = Poll_dataFolder() . $name . '.ips';
+    if (!file_exists($filename)) {
+        touch($filename);
+    }
+    $lines = file($filename);
+    if ($lines === false) {
+        e('cntopen', 'file', $filename);
+        return false;
     }
     $ips = array_map('rtrim', $lines);
     return in_array($_SERVER['REMOTE_ADDR'], $ips);
 }
 
-
 /**
- * Returns whether there's a vote for the given poll.
+ * Returns whether there's a submitted vote for the given poll.
  *
- * @param string $name  The name of the poll.
+ * @param string $name A poll name.
+ *
  * @return bool
  */
-function poll_is_voting($name) {
-    return isset($_POST['poll_'.$name]);
+function Poll_isVoting($name)
+{
+    return isset($_POST['poll_' . $name]);
 }
-
 
 /**
  * Registers the new vote and returns the result view.
  *
- * @param string $name  The name of the poll.
- * @return string  The (X)HTML.
+ * @param string $name A poll name.
+ *
+ * @return string (X)HTML.
+ *
+ * @global array The localization of the plugins.
  */
-function poll_vote($name) {
+function Poll_vote($name)
+{
     global $plugin_tx;
 
     $ptx = $plugin_tx['poll'];
-    $data = poll_data($name);
-    if (count($_POST['poll_'.$name]) > $data['max']) {
-	return sprintf($ptx['error_exceeded_max'], $data['max'])
-		.poll_voting_view($name);
+    $data = Poll_data($name);
+    if (count($_POST['poll_' . $name]) > $data['max']) {
+        return sprintf($ptx['error_exceeded_max'], $data['max'])
+            . Poll_votingView($name);
     }
-    $fn = poll_data_folder().$name.'.ips';
-    if (($fh = fopen($fn, 'a')) !== FALSE
-	    && fwrite($fh, $_SERVER['REMOTE_ADDR']."\n") !== FALSE) {
-	setcookie('poll_'.$name, CMSIMPLE_ROOT, $data['end']);
-	foreach ($_POST['poll_'.$name] as $vote) {
-	    $data['votes'][stsl($vote)]++;
-	}
-	$data['total']++;
-	poll_data($name, $data);
-	$err = FALSE;
+    $filename = Poll_dataFolder() . $name . '.ips';
+    if (($stream = fopen($filename, 'a')) !== false
+        && fwrite($stream, $_SERVER['REMOTE_ADDR'] . PHP_EOL) !== false
+    ) {
+        setcookie('poll_' . $name, CMSIMPLE_ROOT, $data['end']);
+        foreach ($_POST['poll_' . $name] as $vote) {
+            $data['votes'][stsl($vote)]++;
+        }
+        $data['total']++;
+        Poll_data($name, $data);
+        $err = false;
     } else {
-	e('cntwriteto', 'file', $fn);
-	$err = TRUE;
+        e('cntwriteto', 'file', $filename);
+        $err = true;
     }
-    if ($fh !== FALSE) {fclose($fh);}
-    return $err ? poll_voting_view($name)
-	    : $ptx['caption_just_voted'].poll_results_view($name, FALSE);
+    if ($stream !== false) {
+        fclose($stream);
+    }
+    return $err
+        ? Poll_votingView($name)
+        : $ptx['caption_just_voted'] . Poll_resultsView($name, false);
 }
-
 
 /**
  * Returns the voting view.
  *
- * @param string $name  The name of the poll.
- * @return string  The (X)HTML.
+ * @param string $name A poll name.
+ *
+ * @return string (X)HTML.
+ *
+ * @global string The script name.
+ * @global string The current page URL.
+ * @global array  The localization of the plugins.
+ *
+ * @todo Fix empty elements.
  */
-function poll_voting_view($name) {
+function Poll_votingView($name)
+{
     global $sn, $su, $plugin_tx;
 
     $ptx = $plugin_tx['poll'];
-    $data = poll_data($name);
+    $data = Poll_data($name);
     $type = $data['max'] > 1 ? 'checkbox' : 'radio';
-    $o = '<form class="poll" action="'.$sn.'?'.$su.'" method="POST">'."\n"
-	    .$ptx['caption_vote']."\n".'<ul>'."\n";
+    $o = <<<EOT
+<form class="poll" action="$sn?$su" method="post">
+    $ptx[caption_vote]
+    <ul>
+
+EOT;
     $i = 0;
     foreach ($data['votes'] as $key => $dummy) {
-	$o .= '<li>'
-		.tag('input type="'.$type.'" id="poll_'.$name.$i.'" name="poll_'.$name.'[]" value="'.htmlspecialchars($key).'"')
-		.'<label for="poll_'.$name.$i.'">'.$key.'</label></li>'."\n";
-	$i++;
+        $key = htmlspecialchars($key, ENT_COMPAT, 'UTF-8');
+        $o .= <<<EOT
+        <li>
+            <input type="$type" id="poll_$name$i" name="poll_${name}[]"
+                   value="$key" />
+            <label for="poll_$name$i">$key</label>
+        </li>
+
+EOT;
+        $i++;
     }
-    $o .= '</ul>'."\n"
-	    .tag('input type="submit" value="'.$ptx['label_vote'].'"')."\n"
-	    .'</form>'."\n";
+    $o .= <<<EOT
+    </ul>
+    <input type="submit" value="$ptx[label_vote]" />
+</form>
+
+EOT;
     return $o;
 }
-
 
 /**
  * Returns the results view.
  *
- * @param string $name  The name of the poll.
- * @param bool $msg  Whether the caption_voted should be displayed.
- * @return string  The (X)HTML.
+ * @param string $name A poll name.
+ * @param bool   $msg  Whether the caption_voted should be displayed.
+ *
+ * @return string (X)HTML.
+ *
+ * @global string The value of the admin parameter.
+ * @global array  The localization of the core.
  */
-function poll_results_view($name, $msg = TRUE) {
+function Poll_resultsView($name, $msg = true)
+{
     global $admin, $plugin_tx;
 
     $ptx = $plugin_tx['poll'];
-    $data = poll_data($name);
-    $o = $admin == 'plugin_main' ? ''
-	    : (poll_has_ended($name) ? $ptx['caption_ended'] : ($msg ? $ptx['caption_voted'] : ''))."\n"
-		.$ptx['caption_results']."\n";
-    $o .= '<ul class="poll_results">'."\n";
+    $data = Poll_data($name);
+    $o = '';
+    if ($admin != 'plugin_main') {
+        if (Poll_hasEnded($name)) {
+            $o .= $ptx['caption_ended'] . PHP_EOL;
+        } elseif ($msg) {
+            $o .= $ptx['caption_voted'] . PHP_EOL;
+        }
+        $o .= $ptx['caption_results'] . PHP_EOL;
+    }
+    $o .= '<ul class="poll_results">' . PHP_EOL;
     arsort($data['votes']);
     foreach ($data['votes'] as $key => $count) {
-	$percentage = $data['total'] == 0 ? 0 : 100 * $count / $data['total'];
-	$o .= '<li><div class="poll_results">'
-		.sprintf(poll_numerus('label_result', $count), htmlspecialchars($key), $percentage, $count)
-		.'</div>'
-		.'<div class="poll_bar" style="width: '.$percentage.'%">&nbsp;</div></li>'."\n";
+        $percentage = ($data['total'] == 0)
+            ? 0
+            : 100 * $count / $data['total'];
+        $result = sprintf(
+            Poll_number('label_result', $count),
+            htmlspecialchars($key, ENT_COMPAT, 'UTF-8'),
+            $percentage, $count
+        );
+        $o .= <<<EOT
+    <li>
+        <div class="poll_results">$result</div>
+        <div class="poll_bar" style="width: $percentage%">&nbsp;</div>
+    </li>
+
+EOT;
     }
-    $o .= '</ul>'."\n"
-	    .sprintf(poll_numerus('caption_total', $data['total']), $data['total'])."\n";
+    $o .= '</ul>' . PHP_EOL
+        . sprintf(Poll_number('caption_total', $data['total']), $data['total'])
+        . PHP_EOL;
     return $o;
 }
 
 /**
- * Returns the poll view.
+ * Returns the poll view or <var>false</var> in case of an invalid poll name.
+ *
+ * @param string $name A poll name.
+ *
+ * @return string (X)HTML.
+ *
+ * @global string The document fragment containing error messages.
+ * @global array  The localization of the plugins.
  *
  * @access public
- *
- * @param string $name  The name of the poll.
- * @return string  The (X)HTML.
  */
-function poll($name) {
+function poll($name)
+{
     global $e, $plugin_tx;
 
     if (!preg_match('/^[a-z0-9\-]+$/', $name)) {
-	$e = '<li><strong>'.sprintf($plugin_tx['poll']['error_invalid_name'], $name).'</strong></li>'."\n";
-	return FALSE;
+        $e = '<li><b>'
+            . sprintf($plugin_tx['poll']['error_invalid_name'], $name)
+            . '</b></li>' . PHP_EOL;
+        return false;
     }
     $o = '';
-    if (poll_has_ended($name) || poll_has_voted($name)) {
-	$o .= poll_results_view($name);
-    } elseif (poll_is_voting($name)) {
-	$o .= poll_vote($name);
+    if (Poll_hasEnded($name) || Poll_hasVoted($name)) {
+        $o .= Poll_resultsView($name);
+    } elseif (Poll_isVoting($name)) {
+        $o .= Poll_vote($name);
     } else {
-	$o .= poll_voting_view($name);
+        $o .= Poll_votingView($name);
     }
     return $o;
 }
