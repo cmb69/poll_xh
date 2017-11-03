cf309929de2e2eee180e3941cea795fe3bd50599
Christoph M. Becker <cmbecker69@gmx.de>
2017-11-03 22:20:11 +0100

    Remove autoloader – rely on Pfw_XH's instead

--- classes/required_classes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-spl_autoload_register(function ($class) {
-    $parts = explode('_', $class, 2);
-    if ($parts[0] == 'Poll') {
-        include_once __DIR__ . '/' . $parts[1] . '.php';
-    }
-});
