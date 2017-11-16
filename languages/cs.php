d0f3f0056f2cd1b02f040e016489b7535cc8d312
Christoph M. Becker <cmbecker69@gmx.de>
2017-11-16 16:35:14 +0100

    Cater to the changed plural form separator
    Cf. cmb69/pfw_xh@95d358e21e60e55b92510129825cb8d357ea89ba.

--- languages/cs.php
+++ languages/cs.php
@@ -6,9 +6,9 @@
 	$plugin_tx['poll']['caption_ended']="Anketa byla uzavřena.";
 	$plugin_tx['poll']['caption_results']="Výsledky hlasování";
 	$plugin_tx['poll']['label_vote']="Hlasujte!";
-	$plugin_tx['poll']['label_result_0']="%2\$s – %3\$s%% (%1\$s hlas)";
-	$plugin_tx['poll']['label_result_1']="%2\$s – %3\$s%% (%1\$s hlasy)";
-	$plugin_tx['poll']['label_result_2']="%2\$s – %3\$s%% (%1\$s hlasů)";
+	$plugin_tx['poll']['label_result#0']="%2\$s – %3\$s%% (%1\$s hlas)";
+	$plugin_tx['poll']['label_result#1']="%2\$s – %3\$s%% (%1\$s hlasy)";
+	$plugin_tx['poll']['label_result#2']="%2\$s – %3\$s%% (%1\$s hlasů)";
 	$plugin_tx['poll']['error_invalid_name']="Chybný název ankety '%s'! (může obsahovat pouze písmena 'a'-'z', čísla '0'-'9' a místo mezery pomlčku '-')";
 	$plugin_tx['poll']['caption_just_voted']="<p>Děkujeme za váš názor!</p>";
 	$plugin_tx['poll']['error_exceeded_max']="<p class=\"cmsimplecore_warning\">You may check %d options at most!</p>";
--- languages/en.php
+++ languages/en.php
@@ -7,12 +7,12 @@ $plugin_tx['poll']['caption_voted']="You have already voted.";
 $plugin_tx['poll']['caption_just_voted']="<p>Thank you for voting!</p>";
 $plugin_tx['poll']['caption_ended']="The poll has ended.";
 $plugin_tx['poll']['caption_results']="The Results";
-$plugin_tx['poll']['caption_total_0']="In total %s person has voted.";
-$plugin_tx['poll']['caption_total_1']="In total %s people have voted.";
+$plugin_tx['poll']['caption_total#0']="In total %s person has voted.";
+$plugin_tx['poll']['caption_total#1']="In total %s people have voted.";
 
 $plugin_tx['poll']['label_vote']="Vote now!";
-$plugin_tx['poll']['label_result_0']="%2\$s – %3\$s%% (%1\$s vote)";
-$plugin_tx['poll']['label_result_1']="%2\$s – %3\$s%% (%1\$s votes)";
+$plugin_tx['poll']['label_result#0']="%2\$s – %3\$s%% (%1\$s vote)";
+$plugin_tx['poll']['label_result#1']="%2\$s – %3\$s%% (%1\$s votes)";
 
 $plugin_tx['poll']['error_invalid_name']="Invalid poll name '%s'! (must consist of 'a'-'z', '0'-'9' and '-' only)";
 $plugin_tx['poll']['error_exceeded_max']="<p class=\"cmsimplecore_warning\">You may check %d options at most!</p>";
