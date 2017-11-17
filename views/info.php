840e16d1d2f15e9d2817f7ed620a2934e2476800
Christoph M. Becker <cmbecker69@gmx.de>
2017-11-17 11:09:56 +0100

    Re-use back-end styles of Pfw_XH

--- views/info.php
+++ views/info.php
@@ -1,5 +1,5 @@
 <h1>Poll</h1>
-<img src="<?=$logo?>" width="128" height="128" alt="<?=$this->text('alt_logo')?>" class="poll_logo">
+<img src="<?=$logo?>" width="128" height="128" alt="<?=$this->text('alt_logo')?>" class="pfw_logo">
 <p>
     Version: <?=$version?>
 </p>
@@ -7,19 +7,19 @@
     Copyright &copy; 2012-2017 <a href="http://3-magi.net/"
     target="_blank">Christoph M. Becker</a>
 </p>
-<p class="poll_license">
+<p class="pfw_license">
     Poll_XH is free software: you can redistribute it and/or modify it under the
     terms of the GNU General Public License as published by the Free Software
     Foundation, either version 3 of the License, or (at your option) any later
     version.
 </p>
-<p class="poll_license">
+<p class="pfw_license">
     Poll_XH is distributed in the hope that it will be useful, but <em>without
     any warranty</em>; without even the implied warranty of
     <em>merchantibility</em> or <em>fitness for a particular purpose</em>.  See
     the GNU General Public License for more details.
 </p>
-<p class="poll_license">
+<p class="pfw_license">
     You should have received a copy of the GNU General Public License along with
     Poll_XH.  If not, see <a
     href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.
