b0af318a7282876b63f8ad3f7cb907c184a8d87c
Christoph M. Becker <cmbecker69@gmx.de>
2017-11-17 01:30:42 +0100

    Close #5: Add German localization and user manual

---
+++ languages/de.php
@@ -0,0 +1,20 @@
+<?php
+
+$plugin_tx['poll']['menu_main']="Umfragen";
+
+$plugin_tx['poll']['caption_vote']="Bitte stimmen sie ab:";
+$plugin_tx['poll']['caption_voted']="Sie haben bereits abgestimmt.";
+$plugin_tx['poll']['caption_just_voted']="Vielen Dank für Ihre Stimme!";
+$plugin_tx['poll']['caption_ended']="Die Umfrage ist beendet.";
+$plugin_tx['poll']['caption_results']="Die Ergebnisse";
+$plugin_tx['poll']['caption_total#0']="Insgesamt hat %s Person abgestimmt.";
+$plugin_tx['poll']['caption_total#1']="Insgesamt haben %s Leute abgestimmt.";
+
+$plugin_tx['poll']['label_vote']="Jetzt abstimmen!";
+$plugin_tx['poll']['label_result#0']="%2\$s – %3\$s%% (%1\$s Stimme)";
+$plugin_tx['poll']['label_result#1']="%2\$s – %3\$s%% (%1\$s Stimmen)";
+
+$plugin_tx['poll']['error_invalid_name']="Ungültiger Umfragename '%s'! (darf nur 'a'-'z', '0'-'9' und '-' enthalten)";
+$plugin_tx['poll']['error_exceeded_max']="Sie dürften höchstens %d Optionen wählen!";
+
+$plugin_tx['poll']['alt_logo']="Balkendiagramm";
