f6202b4e5c66f419021bdbc46dbd0632335b4b56
Christoph M. Becker <cmbecker69@gmx.de>
2017-11-22 18:53:00 +0100

    Fix CS violations in config and language files

--- languages/cs.php
+++ languages/cs.php
@@ -1,13 +1,13 @@
 <?php
 
-	$plugin_tx['poll']['menu_main']="Ankety";
-	$plugin_tx['poll']['caption_vote']="Hlasujte prosím:";
-	$plugin_tx['poll']['caption_voted']="Již bylo hlasováno.";
-	$plugin_tx['poll']['caption_ended']="Anketa byla uzavřena.";
-	$plugin_tx['poll']['caption_results']="Výsledky hlasování";
-	$plugin_tx['poll']['label_vote']="Hlasujte!";
-	$plugin_tx['poll']['label_result#0']="%2\$s – %3\$s%% (%1\$s hlas)";
-	$plugin_tx['poll']['label_result#1']="%2\$s – %3\$s%% (%1\$s hlasy)";
-	$plugin_tx['poll']['label_result#2']="%2\$s – %3\$s%% (%1\$s hlasů)";
-	$plugin_tx['poll']['error_invalid_name']="Chybný název ankety '%s'! (může obsahovat pouze písmena 'a'-'z', čísla '0'-'9' a místo mezery pomlčku '-')";
-	$plugin_tx['poll']['caption_just_voted']="Děkujeme za váš názor!";
+$plugin_tx['poll']['menu_main']="Ankety";
+$plugin_tx['poll']['caption_vote']="Hlasujte prosím:";
+$plugin_tx['poll']['caption_voted']="Již bylo hlasováno.";
+$plugin_tx['poll']['caption_ended']="Anketa byla uzavřena.";
+$plugin_tx['poll']['caption_results']="Výsledky hlasování";
+$plugin_tx['poll']['label_vote']="Hlasujte!";
+$plugin_tx['poll']['label_result#0']="%2\$s – %3\$s%% (%1\$s hlas)";
+$plugin_tx['poll']['label_result#1']="%2\$s – %3\$s%% (%1\$s hlasy)";
+$plugin_tx['poll']['label_result#2']="%2\$s – %3\$s%% (%1\$s hlasů)";
+$plugin_tx['poll']['error_invalid_name']="Chybný název ankety '%s'! (může obsahovat pouze písmena 'a'-'z', čísla '0'-'9' a místo mezery pomlčku '-')";
+$plugin_tx['poll']['caption_just_voted']="Děkujeme za váš názor!";
