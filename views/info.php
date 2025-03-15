bc16d683fc97660f266af00c1d57a08dca333ded
Christoph M. Becker <cmbecker69@gmx.de>
2025-03-15 16:24:14 +0100

    Drop useless license info from plugin info screen

--- views/info.php
+++ views/info.php
@@ -1,29 +1,4 @@
-<h1>Poll</h1>
-<img src="<?=$logo?>" width="128" height="128" alt="<?=$this->text('alt_logo')?>" class="pfw_logo">
-<p>
-    Version: <?=$version?>
-</p>
-<p>
-    Copyright &copy; 2012-2017 <a href="http://3-magi.net/"
-    target="_blank">Christoph M. Becker</a>
-</p>
-<p class="pfw_license">
-    Poll_XH is free software: you can redistribute it and/or modify it under the
-    terms of the GNU General Public License as published by the Free Software
-    Foundation, either version 3 of the License, or (at your option) any later
-    version.
-</p>
-<p class="pfw_license">
-    Poll_XH is distributed in the hope that it will be useful, but <em>without
-    any warranty</em>; without even the implied warranty of
-    <em>merchantibility</em> or <em>fitness for a particular purpose</em>.  See
-    the GNU General Public License for more details.
-</p>
-<p class="pfw_license">
-    You should have received a copy of the GNU General Public License along with
-    Poll_XH.  If not, see <a
-    href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.
-</p>
+<h1>Poll <?=$this->esc($version)?></h1>
 <div class="poll_syscheck">
     <h2><?=$this->text('syscheck_title')?></h2>
 <?php foreach ($checks as $check):?>
