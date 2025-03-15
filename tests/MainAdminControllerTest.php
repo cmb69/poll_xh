f2fadafff1284a77e680b84b2b4684f3745f5f6f
Christoph M. Becker <cmbecker69@gmx.de>
2025-03-15 17:03:58 +0100

    Test MainAdminController

---
+++ tests/MainAdminControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Poll;
+
+use ApprovalTests\Approvals;
+use PHPUnit\Framework\TestCase;
+use Plib\View;
+
+class MainAdminControllerTest extends TestCase
+{
+    public function testRendersPollResults(): void
+    {
+        $dataService = $this->createStub(DataService::class);
+        $dataService->method("getPollNames")->willreturn(["foo"]);
+        $dataService->method("findPoll")->willReturn($this->poll());
+        $sut = new MainAdminController(
+            $dataService,
+            new View("./views/", XH_includeVar("./languages/en.php", "plugin_tx")["poll"])
+        );
+        Approvals::verifyHtml($sut->defaultAction());
+    }
+
+    private function poll(): Poll
+    {
+        $poll = new Poll();
+        $poll->setVoteCount("Germany", 1);
+        $poll->setVoteCount("Brazil", 0);
+        $poll->setTotalVotes(1);
+        return $poll;
+    }
+}
