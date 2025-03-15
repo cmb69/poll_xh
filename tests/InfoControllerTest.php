7802b2f449a32a31ada1d32ffa91d441e1382854
Christoph M. Becker <cmbecker69@gmx.de>
2025-03-15 16:30:55 +0100

    Test InfoController

---
+++ tests/InfoControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Poll;
+
+use ApprovalTests\Approvals;
+use PHPUnit\Framework\TestCase;
+use Plib\FakeSystemChecker;
+use Plib\View;
+
+class InfoControllerTest extends TestCase
+{
+    public function testRendersPluginInfo(): void
+    {
+        $dataServiceStub = $this->createStub(DataService::class);
+        $dataServiceStub->method("getFolder")->willReturn("./content/poll/");
+        $sut = new InfoController(
+            "./plugins/poll/",
+            $dataServiceStub,
+            new FakeSystemChecker(),
+            new View("./views/", XH_includeVar("./languages/en.php", "plugin_tx")["poll"])
+        );
+        Approvals::verifyHtml($sut->defaultAction());
+    }
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,8 +2,11 @@
 
 require_once './vendor/autoload.php';
 
+require_once '../../cmsimple/functions.php';
+
 require_once '../plib/classes/SystemChecker.php';
 require_once '../plib/classes/View.php';
+require_once '../plib/classes/FakeSystemChecker.php';
 
 require_once './classes/DataService.php';
 require_once './classes/Dic.php';
@@ -11,3 +14,6 @@ require_once './classes/InfoController.php';
 require_once './classes/MainAdminController.php';
 require_once './classes/Poll.php';
 require_once './classes/WidgetController.php';
+
+const CMSIMPLE_XH_VERSION = "CMSimple_XH 1.7.5";
+const POLL_VERSION = "1.0beta3";
